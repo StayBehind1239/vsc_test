338b9fec1e832826344ee2bfeab8030a0118dfad
Yanneck Geiger <yanneck.robo@gmail.com>
2018-11-27 10:16:07 +0000

    Added gsl lib to Project  [working]

--- main.cpp
+++ main.cpp
@@ -1,28 +1,11 @@
-#include <algorithm>
-#include <iomanip>
-#ifndef __GNUC__
-#include <ios>
-#endif
-#include <iostream>
-#include <string>
-#include <vector>
-#include "dice.cpp"
+#include <stdio.h>
+#include <gsl/gsl_sf_bessel.h>
 
-using std::cin;             using std::sort;
-using std::cout;            using std::streamsize;
-using std::endl;            using std::string;
-using std::setprecision;    using std::vector;
-
-int main() {
-
-  int i = 0;
-
-  while(i < 1000000)
-  {
-    cout << "We are at: " << i << "\n";
-
-    i = roll(i);
-
-  }
+int
+main (void)
+{
+  double x = 5.0;
+  double y = gsl_sf_bessel_J0 (x);
+  printf ("J0(%g) = %.18e\n", x, y);
   return 0;
 }
