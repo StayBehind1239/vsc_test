d18f1ccae1d0edcc4048f433b182cb7cf2c3eab7
Yanneck Geiger <yanneck.robo@gmail.com>
2018-11-29 09:37:26 +0000

    Added example from wofram alpha - works

--- main.cpp
+++ main.cpp
@@ -6,6 +6,109 @@
 //  f1 =  -dt1 + sqrt(((x-s2_x)^2)/(s2_v00^2)+((y-s2_y)^2)/(s2_v90^2)) - sqrt(((x-s1_x)^2)/(s1_v00^2)+((y-s1_y)^2)/(s1_v90^2));
 //  f2 =  -dt2 + sqrt(((x-s3_x)^2)/(s3_v00^2)+((y-s3_y)^2)/(s3_v90^2)) - sqrt(((x-s1_x)^2)/(s1_v00^2)+((y-s1_y)^2)/(s1_v90^2));
 
+int print_state_f (size_t iter, gsl_multiroot_fsolver * s)
+{
+  printf ("iter = %3u x = % .3f % .3f "
+          "f(x) = % .3e % .3e\n",
+          iter,
+          gsl_vector_get (s->x, 0),
+          gsl_vector_get (s->x, 1),
+          gsl_vector_get (s->f, 0),
+          gsl_vector_get (s->f, 1));
+}
+
+int print_state_d (size_t iter, gsl_multiroot_fdfsolver * s)
+{
+  printf ("iter = %3u x = % .3f % .3f "
+          "f(x) = % .3e % .3e\n",
+          iter,
+          gsl_vector_get (s->x, 0),
+          gsl_vector_get (s->x, 1),
+          gsl_vector_get (s->f, 0),
+          gsl_vector_get (s->f, 1));
+}
+
+struct wparams
+  {
+    double a;
+    double b;
+    double c;
+    double d;
+  };
+
+int wolfram_f (const gsl_vector * x, void *params,
+              gsl_vector * f)
+{
+  double a = ((struct wparams *) params)->a;
+  double b = ((struct wparams *) params)->b;
+  double c = ((struct wparams *) params)->c;
+  double d = ((struct wparams *) params)->d;
+
+  const double x0 = gsl_vector_get (x, 0);
+  const double x1 = gsl_vector_get (x, 1);
+
+  //  x^2+y^2-1=0 -> x^2+y^2-a=0
+  // (x-2)^2+(y-1)^2-4=0 -> (x-b)^2+(y-c)^2-d=0
+
+  const double y0 = x0*x0 + x1*x1 - a;
+  const double y1 = (x0-b)*(x0-b) + (x1-c)*(x1-c) - d; 
+
+  gsl_vector_set (f, 0, y0);
+  gsl_vector_set (f, 1, y1);
+
+  return GSL_SUCCESS;
+}
+
+/*
+ *  Wolfram Example
+ *  Solver can find both roots depending on the starting point!
+ */
+int main (void)
+{
+  const gsl_multiroot_fsolver_type *T;
+  gsl_multiroot_fsolver *s;
+
+  int status;
+  size_t i, iter = 0;
+
+  const size_t n = 2;
+  struct wparams p = {1.0, 2.0, 1.0, 4.0};
+  gsl_multiroot_function f = {&wolfram_f, n, &p};
+
+  double x_init[2] = {3.0, 3.0};
+  gsl_vector *x = gsl_vector_alloc (n);
+
+  gsl_vector_set (x, 0, x_init[0]);
+  gsl_vector_set (x, 1, x_init[1]);
+
+  T = gsl_multiroot_fsolver_hybrids;
+  s = gsl_multiroot_fsolver_alloc (T, 2);
+  gsl_multiroot_fsolver_set (s, &f, x);
+
+  print_state_f (iter, s);
+
+  do
+    {
+      iter++;
+      status = gsl_multiroot_fsolver_iterate (s);
+
+      print_state_f (iter, s);
+
+      if (status)
+        break;
+
+      status = 
+        gsl_multiroot_test_residual (s->f, 1e-7);
+    }
+  while (status == GSL_CONTINUE && iter < 1000);
+
+  printf ("status = %s\n", gsl_strerror (status));
+
+  gsl_multiroot_fsolver_free (s);
+  gsl_vector_free (x);
+  return 0;
+}
+
 struct mparams
   {
     double dt1;
@@ -109,28 +212,6 @@ int rosenbrock_fdf (const gsl_vector * x, void *params,
   return GSL_SUCCESS;
 }
 
-int print_state_f (size_t iter, gsl_multiroot_fsolver * s)
-{
-  printf ("iter = %3u x = % .3f % .3f "
-          "f(x) = % .3e % .3e\n",
-          iter,
-          gsl_vector_get (s->x, 0),
-          gsl_vector_get (s->x, 1),
-          gsl_vector_get (s->f, 0),
-          gsl_vector_get (s->f, 1));
-}
-
-int print_state_d (size_t iter, gsl_multiroot_fdfsolver * s)
-{
-  printf ("iter = %3u x = % .3f % .3f "
-          "f(x) = % .3e % .3e\n",
-          iter,
-          gsl_vector_get (s->x, 0),
-          gsl_vector_get (s->x, 1),
-          gsl_vector_get (s->f, 0),
-          gsl_vector_get (s->f, 1));
-}
-
 /*
  * Bessel Function Example
  */
@@ -146,7 +227,8 @@ int main (void)
 
 /*
  *  MMEA Solver/Algorithm Example
- */ 
+ */
+/*
 int main (void)
 {
   const gsl_multiroot_fsolver_type *T;
@@ -156,7 +238,7 @@ int main (void)
   size_t i, iter = 0;
 
   const size_t n = 2;
-  // Data from: C:\Users\Development_02\Desktop\Code\Last Day\tests\mtt=1122\comp_data_set_all_corners\test_data_i=1.xlsx
+ // Data from: C:\Users\Development_02\Desktop\Code\Last Day\tests\mtt=1122\comp_data_set_all_corners\test_data_i=1.xlsx
   // From Line 2 ! 
   struct mparams p = {  0.00014164755186999, 0.000690475530648175,    // dt1 & dt2
                         0.00999999999999999, 0.00999999999999999,     // s1_x & s1_y
@@ -187,7 +269,7 @@ int main (void)
 
       print_state_f (iter, s);
 
-      if (status)   /* check if solver is stuck */
+      if (status)   
         break;
 
       status = 
@@ -201,6 +283,7 @@ int main (void)
   gsl_vector_free (x);
   return 0;
 }
+*/
 
 /*
  *  Rosenbrock Example
